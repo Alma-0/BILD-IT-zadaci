98c56271d72e7d1d3b026a2b2b309a9e5c8159b4
Alma_ <Alma_@192.168.1.10>
2017-02-20 11:11:54 +0100

    zadaci_19_02_2017

--- src/zadaci_19_02_2017/Zadatak3.java
+++ src/zadaci_19_02_2017/Zadatak3.java
@@ -6,28 +6,29 @@ public class Zadatak3 {
 		int count = 0;
 		System.out.println("Prime numbers are:\n");
 		// Ispis prostih brojeva u intervalu [2,1000]
-		for (int i = 0; i <= 100000; i++) {
-			if (isPrime(i)) {
+		for (int i = 2; i <= 100000; i++) {
+			if (isPrime(i)==1) {
 				System.out.print(i + " ");
 				count++;
 				// Ukoliko je ispisano 10 brojeva u jednom redu, prelazi u
 				// sljedeci red, radi preglednijeg ispisa
-				if (count % 10 == 0) 
+				if (count % 10 == 0)
 					System.out.println();
 			}
 		}
+		
 
 	}
-
-	public static boolean isPrime(int num) {
+	
+	public static int isPrime(int num) {
 		// Broj je prost broj ukoliko je djeljiv sa 1 i sa samim sobom
 		// Ukoliko je prost broj vraca true, ukoliko nije vraca false
 		for (int i = 2; i < num; i++) {
 			if (num % i == 0) {
-				return false;
+				return 0;
 			}
 		}
-		return true;
+		return 1;
 
 	}
 
