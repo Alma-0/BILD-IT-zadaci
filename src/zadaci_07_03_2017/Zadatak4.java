7d757ca7f10fff1de5094835016a5082625de8cb
Alma_ <Alma_@alma>
2017-03-09 12:54:57 +0100

    zadaci_07_03_2017

--- src/zadaci_07_03_2017/Zadatak4.java
+++ src/zadaci_07_03_2017/Zadatak4.java
@@ -6,20 +6,18 @@ import java.util.Date;
 public class Zadatak4 {
 
 	public static void main(String[] args) {
-		ArrayList<Object> list=new ArrayList<>();
-		Object date=new Date();
-		Object loan=new Loan();
-		Object string=new String("Ponedjeljak");
-		Object circle=new Circle();
-		
+		ArrayList<Object> list = new ArrayList<>();
+		Object date = new Date();
+		Object loan = new Loan();
+		Object string = new String("Ponedjeljak");
+		Object circle = new Circle();
+
 		list.add(date);
 		list.add(loan);
 		list.add(string);
 		list.add(circle);
-		
-		for (int i=0;i<list.size();i++){
-			System.out.println(list.get(i).toString());
-		}
+
+		list.forEach(System.out::println);
 
 	}
 
